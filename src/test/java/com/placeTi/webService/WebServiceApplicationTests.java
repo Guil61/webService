6376ace4fb9bb9beb699d3f64a34e47dc21af1eb
Guil61 <andreguil061@gmail.com>
2024-06-06 18:31:38 -0300

    Begin project

---
+++ src/test/java/com/placeTi/webService/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placeTi.webService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
